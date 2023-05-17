ebd915ba0645a252398debd5e0b58790b6d8216c
osoken <osoken@osoekawalab.com>
2023-05-17 21:05:32 +0900

    fix(logger): fix logger timeformat

--- shikijin/loggers/basic_logger/core.py
+++ shikijin/loggers/basic_logger/core.py
@@ -1,4 +1,4 @@
-from logging import FileHandler, LogRecord, StreamHandler
+from logging import FileHandler, StreamHandler
 from typing import Optional
 
 from pythonjsonlogger.jsonlogger import JsonFormatter
@@ -8,13 +8,13 @@ from ..types import logger_level_type
 
 
 class Formatter(JsonFormatter):
-    default_time_format = "%s"
+    ...
 
 
 class BasicLogger(BaseLogger):
     def __init__(self, name: str, level: logger_level_type = 0, file_path: Optional[str] = None) -> None:
         super(BasicLogger, self).__init__(name, level)
-        formatter = Formatter(fmt="%(asctime)s %(levelname)s", json_ensure_ascii=False)
+        formatter = Formatter(fmt="%(created)f %(asctime)s %(levelname)s", json_ensure_ascii=False)
         stream_handler = StreamHandler()
         stream_handler.setLevel(self.level)
         stream_handler.setFormatter(formatter)
