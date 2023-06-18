d08727972c08e5ddcc8590ec1898d6ea8d85b732
osoken <osoken@osoekawalab.com>
2023-06-18 19:20:33 +0900

    fix(worker): add interface to arguments

--- shikijin/workers/basic_worker/core.py
+++ shikijin/workers/basic_worker/core.py
@@ -1,14 +1,14 @@
 from typing import Optional
 
-from shikijin.fields import ComponentName
-from shikijin.loggers.base import BaseLogger
-
+from ...fields import ComponentName
+from ...interfaces.base import BaseInterface
+from ...loggers.base import BaseLogger
 from ..base import BaseWorker
 
 
 class BasicWorker(BaseWorker):
-    def __init__(self, logger: BaseLogger, name: Optional[ComponentName] = None):
-        super(BasicWorker, self).__init__(logger=logger, name=name)
+    def __init__(self, interface: BaseInterface, logger: BaseLogger, name: Optional[ComponentName] = None):
+        super(BasicWorker, self).__init__(interface=interface, logger=logger, name=name)
 
     def main(self) -> None:
         print(f"hello this is {self.name}")
