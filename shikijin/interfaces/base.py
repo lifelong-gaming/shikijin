cc89657eb8cf4a111f9c52765102de17fe4d6167
osoken <osoken@osoekawalab.com>
2023-06-22 22:27:22 +0900

    feat(interface): add some methods for task assign

--- shikijin/interfaces/base.py
+++ shikijin/interfaces/base.py
@@ -1,7 +1,7 @@
 from abc import ABCMeta, abstractmethod
 
 from ..components import BaseShikijinComponent
-from ..fields import Id
+from ..fields import ComponentId, Id
 from ..types import BaseTask, Blob
 
 
@@ -24,4 +24,12 @@ class BaseInterface(BaseShikijinComponent, metaclass=ABCMeta):
 
     @abstractmethod
     def pickup_task(self) -> BaseTask:
+        ...
+
+    @abstractmethod
+    def signup_task(self, worker_id: ComponentId, task: BaseTask) -> None:
+        ...
+
+    @abstractmethod
+    def signoff_task(self, worker_id: ComponentId, task: BaseTask) -> None:
         ...
