39afc9deb072a56326d56beb222b288a5a3544b0
osoken <osoken@osoekawalab.com>
2023-06-24 23:33:11 +0900

    feat(task): add run's return value

--- shikijin/types.py
+++ shikijin/types.py
@@ -1,4 +1,4 @@
-from collections.abc import Mapping
+from collections.abc import Mapping, Sequence
 from typing import AbstractSet, Any, Callable, Dict, Optional, Union
 
 from humps import camelize
@@ -75,7 +75,7 @@ class BaseEntity(BaseType):
 
 
 class BaseTask(BaseEntity):
-    def run(self) -> None:
+    def run(self) -> Sequence["BaseTask"]:
         raise NotImplementedError
 
 
